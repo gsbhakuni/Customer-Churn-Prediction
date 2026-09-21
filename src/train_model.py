753f7197af82f970d6032155c71b7da321cf1410
Gaurav Singh Bhakuni <gauravbhakuni666@gmail.com>
2026-09-21 16:24:08 +0530

    Added the XGBoost model as well

--- src/train_model.py
+++ src/train_model.py
@@ -1,5 +1,7 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from catboost import CatBoostClassifier
 from sklearn.metrics import accuracy_score
 
 def Logistic_Regression(x_train, x_test, y_train, y_test):
@@ -16,5 +18,13 @@ def Random_Forest(x_train, x_test, y_train, y_test):
     model.fit(x_train, y_train)
     predictions = model.predict(x_test)
     accuracy= accuracy_score(y_test, predictions)
+    accuracy = accuracy * 100
+    return model, round(accuracy, 2)
+
+def XGBoost(x_train, x_test, y_train, y_test):
+    model = XGBClassifier(n_estimators=100, learning_rate=0.1, max_depth = 3, random_state=42)
+    model.fit(x_train, y_train)
+    predict = model.predict(y_train)
+    accuracy = accuracy_score(y_test, predict)
     accuracy = accuracy * 100
     return model, round(accuracy, 2)
