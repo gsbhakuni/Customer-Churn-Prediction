0a40415ff8bae0613000e11b40f07c4e1bcaafb4
Gaurav Singh Bhakuni <gauravbhakuni666@gmail.com>
2026-09-21 16:13:37 +0530

    Used ColumnTransformer instead of LabelEncoder for transforming the data

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,38 +1,29 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
 
 def process_data(df):
     df.drop(columns=['customerID'], inplace=True)
-
-    le = LabelEncoder()
-    col = [
-        "gender",
-        "Partner",
-        "Dependents",
-        "PhoneService",
-        "OnlineSecurity",
-        "OnlineBackup",
-        "DeviceProtection",
-        "TechSupport",
-        "StreamingTV",
-        "StreamingMovies",
-        "PaperlessBilling",
-        "Churn",
-    ]
-    for i in col:
-        df[i] = le.fit_transform(df[i])
-
-    col = ['Contract', 'PaymentMethod', 'InternetService', 'MultipleLines']
-    df = pd.get_dummies(df, columns=col, drop_first=True)
-
-    for i in df.columns:
-        df[i] = le.fit_transform(df[i])
+    df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors="coerce")
+    df["Churn"] = df["Churn"].map({"No" : 0, "Yes" : 1})
 
     y = df['Churn']
-    x = df.drop(columns=['Churn'])
+    x = df.drop("Churn", axis = 1)
 
+    categorical_cols = x.select_dtypes(include="object").columns
+    numerical_cols = x.select_dtypes(exclude="object").columns
+
+    oe = ColumnTransformer(
+        transformers = [
+            ("cat", OneHotEncoder(handle_unknown="ignore"), categorical_cols),
+            ("num", "passthrough", numerical_cols)
+        ]
+    )
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+   
+    x_train = oe.fit_transform(x_train[categorical_cols])
+    x_test = oe.transform(x_test[categorical_cols])
 
     sc = StandardScaler()
     x_train = sc.fit_transform(x_train)
